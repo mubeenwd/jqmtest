19567ef2cf6ceb395b1202e573c3a79f75d74560
mubeenwd <mubeenavailable@gmail.com>
2014-09-24 20:57:45 +0530

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,27 +1,8 @@
-
-var deviceReadyDeferred = $.Deferred();
-var jqmReadyDeferred = $.Deferred();
-
-document.addEventListener("deviceReady", deviceReady, false);
-
-function deviceReady() {
-    deviceReadyDeferred.resolve();
-}
-
-$(document).on("mobileinit", function () {
-    jqmReadyDeferred.resolve();
-});
-
-$.when(deviceReadyDeferred, jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
-
-function doWhenBothFrameworksLoaded() {
-
-    console.log("Initialize jQuery Mobile Phonegap Enhancement Configurations")
-    // Make your jQuery Mobile framework configuration changes here!
-    $.mobile.allowCrossDomainPages = true;
-    $.support.cors = true;
-    $.mobile.buttonMarkup.hoverDelay = 0;
-    $.mobile.pushStateEnabled = false;
-    $.mobile.defaultPageTransition = "none";
-    // TBD
+function showMessage(msg) {
+    navigator.notification.alert(
+        msg,
+        function () { },
+        'Sarkar - Vidhansabha 2014',
+        'Ok'
+    );
 }
